0bb3d94eeb2fda6e68c0a790771d7ca467245499
jinminki <tymikijn@naver.com>
2024-09-12 16:07:22 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const cors = require('cors');  // CORS 미들웨어 추가
 const app = express();
+
+// CORS 설정 (Netlify에서 요청을 허용)
+app.use(cors({
+    origin: 'https://cute-muffin-a1da21.netlify.app',  // Netlify에서 오는 요청 허용
+    methods: ['GET', 'POST'],  // 허용할 HTTP 메소드 지정
+    allowedHeaders: ['Content-Type'],  // 허용할 헤더 지정
+}));
 
 app.use(express.json());  // JSON 형식의 요청을 처리
 
@@ -11,7 +19,7 @@ const dataFilePath = path.join(__dirname, 'zoneData.json');
 // 구역 정보 저장 API
 app.post('/save-zone', (req, res) => {
     const { zoneName, zoneFloor, zoneDetail } = req.body;
-    
+
     // 입력된 구역 데이터를 JSON 파일로 저장
     const data = { zoneName, zoneFloor, zoneDetail };
     fs.writeFileSync(dataFilePath, JSON.stringify(data, null, 2));
